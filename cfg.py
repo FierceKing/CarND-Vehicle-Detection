104f239a104402b02134ef97e6cb2fa00057d1f0
FierceKing <mastemag@gmail.com>
2017-11-18 16:11:40 +0800

    successfully seperate train and test

---
+++ cfg.py
@@ -0,0 +1,23 @@
+"""
+Save all global variables here
+"""
+# Read in cars and notcars
+folder_car = 'data/vehicles'
+folder_notcar = 'data/non-vehicles'
+
+# Reduce the sample size because
+# The quiz evaluator times out after 13s of CPU time
+sample_size = -1
+
+### TODO: Tweak these parameters and see how the results change.
+color_space = 'RGB'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+orient = 9  # HOG orientations
+pix_per_cell = 8  # HOG pixels per cell
+cell_per_block = 2  # HOG cells per block
+hog_channel = 0  # Can be 0, 1, 2, or "ALL"
+spatial_size = (16, 16)  # Spatial binning dimensions
+hist_bins = 16  # Number of histogram bins
+spatial_feat = True  # Spatial features on or off
+hist_feat = True  # Histogram features on or off
+hog_feat = True  # HOG features on or off
+y_start_stop = [None, None]  # Min and max in y to search in slide_window()
--- train_clf.py
+++ train_clf.py
@@ -6,17 +6,21 @@ import glob
 import time
 from sklearn.svm import LinearSVC
 from sklearn.preprocessing import StandardScaler
+from sklearn.externals import joblib
 from skimage.feature import hog
-
-from cfg import *
-
 from lesson_functions import load_images,\
                             get_hog_features, bin_spatial, color_hist,\
                             extract_features, slide_window, draw_boxes
+# import lesson_functions
+# import inspect
+# all_functions = inspect.getmembers(lesson_functions, inspect.isfunction)
+# funcs = [funtuple[0] for funtuple in all_functions]
+# xx, yy = [eval('lesson_functions.{}'.format(func)) for func in funcs]
 
 # NOTE: the next import is only valid for scikit-learn version <= 0.17
 # for scikit-learn >= 0.18 use:
 from sklearn.model_selection import train_test_split
+
 
 
 # Define a function to extract features from a single image window
@@ -103,104 +107,82 @@ def search_windows(img, windows, clf, scaler, color_space='RGB',
 
 
 def main():
-    # train_flag = True
-    # # Read in cars and notcars
-    # folder_car = 'data/vehicles'
-    # folder_notcar = 'data/non-vehicles'
-    # cars = load_images(folder_car)
-    # notcars = load_images(folder_notcar)
-    #
-    # # Reduce the sample size because
-    # # The quiz evaluator times out after 13s of CPU time
-    # sample_size = -1
-    # cars = cars[0:sample_size]
-    # notcars = notcars[0:sample_size]
-    #
-    # ### TODO: Tweak these parameters and see how the results change.
-    # color_space = 'RGB'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
-    # orient = 9  # HOG orientations
-    # pix_per_cell = 8  # HOG pixels per cell
-    # cell_per_block = 2  # HOG cells per block
-    # hog_channel = 0  # Can be 0, 1, 2, or "ALL"
-    # spatial_size = (16, 16)  # Spatial binning dimensions
-    # hist_bins = 16  # Number of histogram bins
-    # spatial_feat = True  # Spatial features on or off
-    # hist_feat = True  # Histogram features on or off
-    # hog_feat = True  # HOG features on or off
-    # y_start_stop = [None, None]  # Min and max in y to search in slide_window()
-    #
-    # car_features = extract_features(cars, color_space=color_space,
-    #                                 spatial_size=spatial_size, hist_bins=hist_bins,
-    #                                 orient=orient, pix_per_cell=pix_per_cell,
-    #                                 cell_per_block=cell_per_block,
-    #                                 hog_channel=hog_channel, spatial_feat=spatial_feat,
-    #                                 hist_feat=hist_feat, hog_feat=hog_feat)
-    # notcar_features = extract_features(notcars, color_space=color_space,
-    #                                    spatial_size=spatial_size, hist_bins=hist_bins,
-    #                                    orient=orient, pix_per_cell=pix_per_cell,
-    #                                    cell_per_block=cell_per_block,
-    #                                    hog_channel=hog_channel, spatial_feat=spatial_feat,
-    #                                    hist_feat=hist_feat, hog_feat=hog_feat)
-    #
-    # X = np.vstack((car_features, notcar_features)).astype(np.float64)
-    # # Fit a per-column scaler
-    # X_scaler = StandardScaler().fit(X)
-    #
-    # # Apply the scaler to X
-    # scaled_X = X_scaler.transform(X)
-    #
-    # # Define the labels vector
-    # y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
-    #
-    # # Split up data into randomized training and test sets
-    # rand_state = np.random.randint(0, 100)
-    # X_train, X_test, y_train, y_test = train_test_split(
-    #     scaled_X, y, test_size=0.2, random_state=rand_state)
-    #
-    # print('Using:', orient, 'orientations', pix_per_cell,
-    #       'pixels per cell and', cell_per_block, 'cells per block')
-    # print('Feature vector length:', len(X_train[0]))
-    # # Use a linear SVC
-    # svc = LinearSVC()
-    # # Check the training time for the SVC
-    # t = time.time()
-    # svc.fit(X_train, y_train)
-    # t2 = time.time()
-    # print(round(t2 - t, 2), 'Seconds to train SVC...')
-    # # Check the score of the SVC
-    # print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
-    # # Check the prediction time for a single sample
-    # t = time.time()
+    train_flag = True
+    # Read in cars and notcars
+    folder_car = 'data/vehicles'
+    folder_notcar = 'data/non-vehicles'
+    cars = load_images(folder_car)
+    notcars = load_images(folder_notcar)
 
-    from sklearn.externals import joblib
+    # Reduce the sample size because
+    # The quiz evaluator times out after 13s of CPU time
+    sample_size = -1
+    cars = cars[0:sample_size]
+    notcars = notcars[0:sample_size]
+
+    ### TODO: Tweak these parameters and see how the results change.
+    color_space = 'RGB'  # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+    orient = 9  # HOG orientations
+    pix_per_cell = 8  # HOG pixels per cell
+    cell_per_block = 2  # HOG cells per block
+    hog_channel = 0  # Can be 0, 1, 2, or "ALL"
+    spatial_size = (16, 16)  # Spatial binning dimensions
+    hist_bins = 16  # Number of histogram bins
+    spatial_feat = True  # Spatial features on or off
+    hist_feat = True  # Histogram features on or off
+    hog_feat = True  # HOG features on or off
+    y_start_stop = [None, None]  # Min and max in y to search in slide_window()
+
+    car_features = extract_features(cars, color_space=color_space,
+                                    spatial_size=spatial_size, hist_bins=hist_bins,
+                                    orient=orient, pix_per_cell=pix_per_cell,
+                                    cell_per_block=cell_per_block,
+                                    hog_channel=hog_channel, spatial_feat=spatial_feat,
+                                    hist_feat=hist_feat, hog_feat=hog_feat)
+    notcar_features = extract_features(notcars, color_space=color_space,
+                                       spatial_size=spatial_size, hist_bins=hist_bins,
+                                       orient=orient, pix_per_cell=pix_per_cell,
+                                       cell_per_block=cell_per_block,
+                                       hog_channel=hog_channel, spatial_feat=spatial_feat,
+                                       hist_feat=hist_feat, hog_feat=hog_feat)
+
+    X = np.vstack((car_features, notcar_features)).astype(np.float64)
+    # Fit a per-column scaler
+    X_scaler = StandardScaler().fit(X)
     scaler_filename = 'X_scaler.pkl'
+    joblib.dump(X_scaler, scaler_filename)
+    print("The trained X_scaler is saved to: ", scaler_filename)
+
+    # Apply the scaler to X
+    scaled_X = X_scaler.transform(X)
+
+    # Define the labels vector
+    y = np.hstack((np.ones(len(car_features)), np.zeros(len(notcar_features))))
+
+    # Split up data into randomized training and test sets
+    rand_state = np.random.randint(0, 100)
+    X_train, X_test, y_train, y_test = train_test_split(
+        scaled_X, y, test_size=0.2, random_state=rand_state)
+
+    print('Using:', orient, 'orientations', pix_per_cell,
+          'pixels per cell and', cell_per_block, 'cells per block')
+    print('Feature vector length:', len(X_train[0]))
+    # Use a linear SVC
+    svc = LinearSVC()
+    # Check the training time for the SVC
+    t = time.time()
+    svc.fit(X_train, y_train)
+    t2 = time.time()
+    print(round(t2 - t, 2), 'Seconds to train SVC...')
+    # Check the score of the SVC
+    print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
+    # Check the prediction time for a single sample
+    t = time.time()
+
+
     clf_filename = 'svc.pkl'
-    X_scaler = joblib.load(scaler_filename)
-    svc = joblib.load(clf_filename)
-
-    image = mpimg.imread('test_images/test4.jpg')
-    draw_image = np.copy(image)
-
-    # Uncomment the following line if you extracted training
-    # data from .png images (scaled 0 to 1 by mpimg) and the
-    # image you are searching is a .jpg (scaled 0 to 255)
-    image = image.astype(np.float32)/255
-
-    windows = slide_window(image, x_start_stop=[None, None], y_start_stop=y_start_stop,
-                           xy_window=(120, 120), xy_overlap=(0.5, 0.5))
-
-    hot_windows = search_windows(image, windows, svc, X_scaler, color_space=color_space,
-                                 spatial_size=spatial_size, hist_bins=hist_bins,
-                                 orient=orient, pix_per_cell=pix_per_cell,
-                                 cell_per_block=cell_per_block,
-                                 hog_channel=hog_channel, spatial_feat=spatial_feat,
-                                 hist_feat=hist_feat, hog_feat=hog_feat)
-
-    window_img = draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)
-
-    plt.imshow(window_img)
-    plt.show()
-
+    joblib.dump(svc, clf_filename)
+    print("The trained classifier is saved to: ", clf_filename)
 
 if __name__ == '__main__':
     main()
